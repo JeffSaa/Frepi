268d287311ef417c4575fa42f3d4b6cdd2c81aaa
Andres Castañeda Borre <borref22@gmail.com>
2015-08-07 21:03:03 -0500

    login, first sketch

--- frepiFront/config.rb
+++ frepiFront/config.rb
@@ -47,11 +47,15 @@
 #   end
 # end
 
+require "slim"
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :layout, false
 
 # Build-specific configuration
 configure :build do
