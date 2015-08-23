acedeca7ed7d7b03b644bf5fee51c6efb6742a9f
Alberto Camargo <amcamargo95@gmail.com>
2015-08-23 16:10:18 -0500

    disable security temporal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   before_action :set_access_control_headers
 
   # Security
-  before_action :authenticate_user!, :require_administrator, except: [:handle_options_request, :set_access_control_headers]
-  skip_before_action :authenticate_user!, :require_administrator, if: :devise_controller?
+  #before_action :authenticate_user!, :require_administrator, except: [:handle_options_request, :set_access_control_headers]
+  #skip_before_action :authenticate_user!, :require_administrator, if: :devise_controller?
 
 
   def handle_options_request
