95a7d921ef528b05e58b369328d5a9aaf1d4f1fe
Alberto Camargo <amcamargo95@gmail.com>
2015-10-20 12:24:53 -0500

    test for cities controller

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -1,29 +1,114 @@
 require 'test_helper'
 
 class CitiesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+
+  # ---------------- Index --------------------- #
+
+  test "clients and shoppers should not index cities" do
+    sign_in :user, users(:user)
+    get :index, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+    assert_response :unauthorized
+
+    sign_out users(:user)
+
+    sign_in :shopper, shoppers(:shopper)
+    get :index, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+    assert_response :unauthorized
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
+
+  test "administrator should index cities" do
+    sign_in :user, users(:admin)
+
+    get :index, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+    assert_response :ok
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  # ---------------- Show ----------------------- #
+
+  test "administrator should show a city" do
+    sign_in :user, users(:admin)
+    get :show, id: cities(:barranquilla).id, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+
+    assert_response :ok
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "clients and shoppers should not show a city" do
+    sign_in :user, users(:user)
+    get :show, { id: cities(:palermo).id, country_id: countries(:italy).id,  state_id: states(:sicilia).id }
+    assert_response :unauthorized
+
+    sign_out users(:user)
+
+    sign_in :shopper, shoppers(:shopper)
+    get :show, { id: cities(:palermo).id, country_id: countries(:italy).id,  state_id: states(:sicilia).id }
+    assert_response :unauthorized
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  # ---------------- Create ----------------------- #
+
+  test "administrator should create a city" do
+    sign_in :user, users(:admin)
+    post :create, { name: 'Ponedera', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+
+    assert_response :created
+  end
+
+
+  test "clients and shoppers should not create a city" do
+    sign_in :user, users(:user)
+    post :create, { name: 'Ponedera', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+    assert_response :unauthorized
+
+    sign_out users(:user)
+
+    sign_in :shopper, shoppers(:shopper)
+    post :create, { name: 'Ponedera', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+    assert_response :unauthorized
+  end
+
+  # ---------------- Update ----------------------- #
+
+  test "administrator should update a city" do
+    sign_in :user, users(:admin)
+    put :update, { id: cities(:barranquilla).id, name: 'barranquilla 1', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+
+    assert_response :ok
+  end
+
+
+  test "clients and shoppers should not update a city" do
+    sign_in :user, users(:user)
+    put :update, { id: cities(:barranquilla).id, name: 'barranquilla 1', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+    assert_response :unauthorized
+
+    sign_out users(:user)
+
+    sign_in :shopper, shoppers(:shopper)
+    put :update, { id: cities(:barranquilla).id, name: 'barranquilla 1', country_id: countries(:colombia).id, state_id: states(:atlantico).id }
+    assert_response :unauthorized
+  end
+
+  # ---------------- Destroy ----------------------- #
+
+  test "administrator should destroy a city" do
+    sign_in :user, users(:admin)
+    delete :destroy, id: cities(:barranquilla).id, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+
+    assert_response :ok
+  end
+
+
+  test "clients and shoppers should not destroy a city" do
+    sign_in :user, users(:user)
+    delete :destroy, id: cities(:barranquilla).id, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+    assert_response :unauthorized
+
+    sign_out users(:user)
+
+    sign_in :shopper, shoppers(:shopper)
+    delete :destroy, id: cities(:barranquilla).id, country_id: countries(:colombia).id, state_id: states(:atlantico).id
+    assert_response :unauthorized
   end
 
 end
