02664613a944b7b012e3f5d6579d6346b5ded8bb
Alberto Camargo <amcamargo95@gmail.com>
2015-11-06 18:17:27 -0500

    minor error in serializer

--- app/serializers/explorer_category_serializer.rb
+++ app/serializers/explorer_category_serializer.rb
@@ -1,8 +1,13 @@
 class ExplorerCategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :description
   has_many   :subcategories, serializer: ExplorerSubcategorySerializer
+  has_many   :products
 
   def subcategories
     object.subcategories.distinct
+  end
+
+  def products
+    object.products.last(5)
   end
 end
--- app/serializers/explorer_subcategory_serializer.rb
+++ app/serializers/explorer_subcategory_serializer.rb
@@ -1,8 +1,3 @@
 class ExplorerSubcategorySerializer < ActiveModel::Serializer
   attributes :id, :category_id, :name
-  has_many  :products
-
-  def products
-    object.products.last(5)
-  end
 end
