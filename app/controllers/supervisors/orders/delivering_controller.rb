9c24e66511ef94d5bf5951373897ae758025d7f6
Alberto Camargo <amcamargo95@gmail.com>
2015-12-05 20:03:22 -0500

    sort orders according expiry time and scheduled date

--- app/controllers/supervisors/orders/delivering_controller.rb
+++ app/controllers/supervisors/orders/delivering_controller.rb
@@ -2,6 +2,6 @@ class Supervisors::Orders::DeliveringController < ApplicationController
   skip_before_action :authenticate_user!, :require_administrator
 
   def index
-    render json: Order.where(active: true, status: 2), each_serializer: SupervisorOrderSerializer
+    render json: Order.where(active: true, status: 2).order(:scheduled_date, :expiry_time), each_serializer: SupervisorOrderSerializer
   end
 end
--- app/controllers/supervisors/orders/received_controller.rb
+++ app/controllers/supervisors/orders/received_controller.rb
@@ -2,6 +2,6 @@ class Supervisors::Orders::ReceivedController < ApplicationController
   skip_before_action :authenticate_user!, :require_administrator
 
   def index
-    render json: Order.where(active: true, status: 0), each_serializer: SupervisorOrderSerializer
+    render json: Order.where(active: true, status: 0).order(:scheduled_date, :expiry_time), each_serializer: SupervisorOrderSerializer
   end
 end
