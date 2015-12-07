b1a2f6032f03aec10a7ed5f0d935339e365991ab
Alberto Camargo <amcamargo95@gmail.com>
2015-12-06 19:35:40 -0500

    added cross origin methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::Serialization
+
+  # Api connection
+  before_action :set_access_control_headers
+
+  # Methods
+  def handle_options_request
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, uid, access-token, client'
+    headers['Access-Control-Expose-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, uid, access-token, client'
+  end
 end
