eb2071dc6e1532f63eabe249f182e85e66b1b24d
Alberto Camargo <amcamargo95@gmail.com>
2016-01-15 13:35:26 -0500

    added pagination for controller statistics

--- app/controllers/api/v1/administrator/statistics/products_controller.rb
+++ app/controllers/api/v1/administrator/statistics/products_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Administrator::Statistics::ProductsController < Api::V1::ApiContr
   skip_before_action  :authenticate_user!, :require_administrator
 
   def index
-    if params[:start_date] || params[:end_date]
+    if params[:start_date] && params[:end_date] && params[:page]
 
       orders = Order.where("status > ? AND active = ?", 0, true).where("shopping_at >= ? AND shopping_at <= ?", params[:start_date], params[:end_date])
       products = Hash.new(0)
@@ -15,9 +15,15 @@ class Api::V1::Administrator::Statistics::ProductsController < Api::V1::ApiContr
 
       products = products.sort_by { |product, quantity| product.sales_count = quantity; quantity }.reverse.to_h.keys
 
-      render json: products, each_serializer: ProductSerializer
+      @products = WillPaginate::Collection.create(params[:page], params[:per_page], products.length) do |pager|
+        pager.replace products[pager.offset, pager.per_page]
+      end
+
+      set_pagination_headers :products
+      render json: @products, each_serializer: ProductSerializer
+
     else
-      head(:bad_request)
+      render json: { error: "param 'page' or 'start_date' or 'end_date' has not been found" }, status: :bad_request
     end
   end
 end
