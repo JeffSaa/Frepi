dc70bc8e118516797a37691358d75192d8023bf8
Alberto Camargo <amcamargo95@gmail.com>
2016-02-05 05:47:30 +0000

    added category to products

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,15 +1,15 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :reference_code, :name, :store_price, :frepi_price, :image,
-             :available, :sales_count, :subcategory_id, :category
+             :available, :sales_count#, :subcategory_id
 
-  has_one    :sucursal
-
+  has_one    :sucursal, :subcategory, :category
+  
   def sucursal
     object.sucursals.first
   end
 
-
   def category
   	object.category
   end
+
 end
