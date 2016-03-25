31b21babaa2b90ec130bd48ae55ea2cf5036836c
Alberto Camargo <amcamargo95@gmail.com>
2016-03-25 01:33:06 +0000

    added attributes in orders supervisor controllers

--- app/serializers/supervisor_order_serializer.rb
+++ app/serializers/supervisor_order_serializer.rb
@@ -1,5 +1,8 @@
 class SupervisorOrderSerializer < ActiveModel::Serializer
-  attributes :id, :active, :status, :total_price, :date, :scheduled_date, :arrival_time, :expiry_time, :comment, :address
+  attributes :id, :active, :status, :total_price, :date,
+             :scheduled_date, :arrival_time, :expiry_time, 
+             :comment, :address, :shopping_at, :notification_email,
+             :telephone
 
   has_many :products
   has_one  :user
--- test/controllers/api/v1/supervisors/orders_controller_test.rb
+++ test/controllers/api/v1/supervisors/orders_controller_test.rb
@@ -22,4 +22,39 @@ class  Api::V1::Supervisors::OrdersControllerTest < ActionController::TestCase
     assert_response :created
   end
 
+
+  test "An supervisor return a order to received status" do
+    total = orders(:one).total_price
+    quantity_jhonny = products(:johnny).sales_count
+    quantity_jack = products(:jack).sales_count
+    shoppers = orders(:one).shopper
+    sales_jhonny = Product.find(products(:johnny).id).sales_count
+    sales_jack = Product.find(products(:jack).id).sales_count
+
+    sign_in :supervisor, supervisors(:supervisor)
+    delete :destroy, id: orders(:one)
+    response = JSON.parse(@response.body)
+    
+
+    assert_equal(total, response['totalPrice'].to_f)
+    assert_equal(true, response['active'])
+    assert_match("RECEIVED", response['status'])
+    assert_equal(sales_jack - quantity_jack, Product.find(products(:jack).id).sales_count)
+    assert_equal(sales_jhonny - quantity_jhonny, Product.find(products(:johnny).id).sales_count)
+  end
+
+  test "An supervisor can disable a order if the status is RECEIVED" do
+    total = orders(:two).total_price
+    shoppers = orders(:two).shopper
+    
+
+    sign_in :supervisor, supervisors(:supervisor)
+    delete :destroy, id: orders(:two)
+    response = JSON.parse(@response.body)
+    
+
+    assert_equal(false, response['active'])
+    assert_match("RECEIVED", response['status'])
+  end
+
 end
