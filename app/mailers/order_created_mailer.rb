bf734bb663a643fd3e69a718e51bbd9c4cf75d2c
Alberto Camargo <amcamargo95@gmail.com>
2016-07-12 19:16:26 +0000

    I forget uncommment a line

--- app/mailers/order_created_mailer.rb
+++ app/mailers/order_created_mailer.rb
@@ -1,7 +1,7 @@
 class OrderCreatedMailer < ApplicationMailer
 
-  #ADMIN = ['ernestodelae@frepi.com.co', 'butron4@hotmail.com', 'amcamargo95@gmail.com', 'borref22@gmail.com', 'pablobutca@gmail.com']
-  ADMIN = ['amcamargo95@gmail.com']
+  ADMIN = ['ernestodelae@frepi.com.co', 'butron4@hotmail.com', 'amcamargo95@gmail.com', 'borref22@gmail.com', 'pablobutca@gmail.com']
+  #ADMIN = ['amcamargo95@gmail.com']
 
   def notification_email(order, user)
     @order = order
