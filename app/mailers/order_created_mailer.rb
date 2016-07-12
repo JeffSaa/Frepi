ca3a5ed9b97330d5a78e86b489f1a1be72cde359
Alberto Camargo <amcamargo95@gmail.com>
2016-07-12 18:21:41 +0000

    Email confirmation and minor changes

--- app/mailers/order_created_mailer.rb
+++ app/mailers/order_created_mailer.rb
@@ -1,10 +1,19 @@
 class OrderCreatedMailer < ApplicationMailer
 
   ADMIN = ['ernestodelae@frepi.com.co', 'butron4@hotmail.com', 'amcamargo95@gmail.com', 'borref22@gmail.com', 'pablobutca@gmail.com']
+  #ADMIN = ['amcamargo95@gmail.com']
 
   def notification_email(order, user)
     @order = order
     @user = user
     mail to: ADMIN, subject: 'Orden Creada!'
+  end
+
+
+  def notification_client(order, user)
+    @order = order
+    @user = user
+    mail to: user.email, subject: 'Gracias por tu compra'
+
   end
 end
