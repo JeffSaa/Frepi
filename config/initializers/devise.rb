05657ae1436e5857335d1f55d8f00821003b9e06
Alberto Camargo <amcamargo95@gmail.com>
2016-07-18 04:50:15 +0000

    change language for email recover password

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   #config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.mailer_sender = "support@myapp.com"
+  config.mailer_sender = "noreply@frepi.co"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
