08c781ee0419f41c9ac31523e148b4e84744af9d
Alberto Camargo <amcamargo95@gmail.com>
2016-07-23 01:27:47 +0000

    minor changes

--- app/serializers/supervisor_order_serializer.rb
+++ app/serializers/supervisor_order_serializer.rb
@@ -2,8 +2,7 @@ class SupervisorOrderSerializer < ActiveModel::Serializer
   attributes :id, :active, :status, :total_price, :date,
              :scheduled_date, :arrival_time, :expiry_time, 
              :comment, :address, :shopping_at, :notification_email,
-             :telephone, :discount
-
+             :telephone, :discount, :payment
   has_many :products
   has_one  :user
   has_many :shopper
