f83ebec73382c2c629754ea693585e3c59a3fc7a
Roberto Daza Castro <arjayx2011@gmail.com>
2016-10-11 12:02:37 -0500

    added business update resources

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -9,13 +9,13 @@ class Api::V1::BusinessesController <  Api::V1::ApiController
     page = params[:page] || 1
     per_page = params[:per_page] || 10
     businesses =
-                  if params[:start_with].present?
-                     Business.start_with(params[:start_with]).paginate(page: page, per_page: per_page)
-                   elsif params[:nit].present?
-                     Business.nit(params[:nit]).paginate(page: page, per_page: per_page)
-                   else
-                     Business.all.paginate(page: params[:page], per_page: 10)
-                   end
+                if params[:start_with].present?
+                  Business.start_with(params[:start_with]).paginate(page: page, per_page: per_page)
+                elsif params[:nit].present?
+                  Business.nit(params[:nit]).paginate(page: page, per_page: per_page)
+                else
+                  Business.all.paginate(page: params[:page], per_page: 10)
+                end
     render json: businesses, status: :created
   end
 
